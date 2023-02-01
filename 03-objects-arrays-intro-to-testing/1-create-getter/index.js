6bb5f974a2f9795bc5663b28f410f581c3e0f11f
Сергей <skaluzhni@sutochno.ru>
2023-02-01 13:56:15 +0300

    [04-oop-basic-intro-to-dom]

--- 03-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 03-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -12,30 +12,25 @@ export function createGetter(path) {
     isLastElem: (idx, arr) => idx === (arr.length - 1)
   };
 
-  //Кэшируем ключи и результат
+  //Кэшируем ключи
   const keys = path.split('.');
-  let result;
-
-  //Рекурсивная функция для поиска и возврата поля
-  function recursive (target) {
-    keys.forEach((key, idx, keys) => {
-      if (utils.isObject(target) &&
-        Object.hasOwn(target, key) &&
-        utils.isObject(target[key]) &&
-        !utils.isLastElem(idx, keys)) {
-        recursive(target[key]);
-      }
-      if (Object.hasOwn(target, key) &&
-        utils.isLastElem(idx, keys)) {
-        result = target[key];
-      }
-    });
-  }
 
   return (obj) => {
-    //Если в кэше есть результат, то возвращаем его, экономя память
-    if (result) {
-      return result;
+    let result;
+    //Рекурсивная функция для поиска и возврата поля
+    function recursive (target) {
+      keys.forEach((key, idx, keys) => {
+        if (utils.isObject(target) &&
+          Object.hasOwn(target, key) &&
+          utils.isObject(target[key]) &&
+          !utils.isLastElem(idx, keys)) {
+          recursive(target[key]);
+        }
+        if (Object.hasOwn(target, key) &&
+          utils.isLastElem(idx, keys)) {
+          result = target[key];
+        }
+      });
     }
     recursive(obj);
     return result;
--- 04-oop-basic-intro-to-dom/1-column-chart/index.js
+++ 04-oop-basic-intro-to-dom/1-column-chart/index.js
@@ -1,3 +1,87 @@
 export default class ColumnChart {
+  chartHeight = 50;
+  element = {}
+  constructor({
+    data = [],
+    label = '',
+    value = 0,
+    link = '',
+    formatHeading = v => v
+  } = {}) {
+    this.data = data;
+    this.label = label;
+    this.value = formatHeading(value);
+    this.link = link;
 
+    this.render();
+  }
+
+  getTemplate() {
+    return `
+      <div class="column-chart column-chart_loading" style="--chart-height: ${this.chartHeight}">
+        <div class="column-chart__title">
+          Total: ${this.label}
+          ${this.getLink()}
+        </div>
+        <div class="column-chart__container">
+          <div data-element="header" class="column-chart__header">${this.value}</div>
+          <div data-element="body" class="column-chart__chart">
+            ${this.getColumnProps()}
+          </div>
+        </div>
+      </div>
+    `;
+  }
+
+  render() {
+    const element = document.createElement("div"); // (*)
+
+    element.innerHTML = this.getTemplate();
+
+    // NOTE: в этой строке мы избавляемся от обертки-пустышки в виде `div`
+    // который мы создали на строке (*)
+    this.element = element.firstElementChild;
+
+    if (this.data.length > 0) {
+      this.element.classList.remove("column-chart_loading");
+    }
+
+  }
+
+  getColumnProps() {
+    const maxValue = Math.max(...this.data);
+    const scale = this.chartHeight / maxValue;
+
+    return this.data.map(item => {
+      const percent = ((item / maxValue) * 100).toFixed(0);
+
+      return `<div style="--value: ${Math.floor(item * scale)}" data-tooltip="${percent}%"></div>`;
+    }).join('');
+  }
+
+  getLink() {
+    if (this.link) {
+      return `<a href="${this.link}" class="column-chart__link">View all</a>`;
+    }
+    return '';
+  }
+
+  update(data) {
+    this.data = data;
+    this.element
+      .querySelector('[data-element="body"]')
+      .innerHTML = this.getColumnProps();
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+    }
+  }
+
+  destroy() {
+    this.remove();
+    this.element = {}
+    // NOTE: удаляем обработчики событий, если они есть
+  }
 }
